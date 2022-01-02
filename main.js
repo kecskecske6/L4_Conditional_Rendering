3664b743189a81346804c7ddff2533e67876a40f
nitslaszlo <nitslaszlo@gmail.com>
2022-01-02 09:23:20 +0100

    settings

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            // solution
-            onSale: true
-            // solution
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inventory: 100,
+      // solution
+      onSale: true,
+      // solution
+    };
+  },
+});
